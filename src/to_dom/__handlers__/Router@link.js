3c476e64aed3d399423682ee154cc20860518ca6
5alidz <kzack-dev@hotmail.com>
2019-09-16 04:08:43 +0300

    remove default handlers remaining files

--- src/to_dom/__handlers__/Router@link.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  href: {
-    type: 'string',
-    required: true,
-  }
-}
--- src/to_dom/handlers/Router@head.js
+++
@@ -1,8 +0,0 @@
-import g from './shared/router.js'
-
-export default function router_head_handler(vNode, { to_dom }) {
-  const current_route = window.location.pathname
-  const head_array = vNode.children.map(child => to_dom(child))
-  g.heads[current_route] = head_array
-  g.heads[current_route].map(_ => g.doc_head.appendChild(_))
-}
