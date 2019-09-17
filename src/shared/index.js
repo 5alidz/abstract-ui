5a56c398f64dc1ad98beaaae107a1595a9a71674
5alidz <kzack-dev@hotmail.com>
2019-09-17 11:36:04 +0300

    fix types from nano_spa to abstract-ui

--- src/shared/index.js
+++ src/shared/index.js
@@ -2,10 +2,10 @@ export const typeOf = object => Object.prototype.toString.call(object)
   .replace(/[[\]]/g, '').split(' ')[1].toLowerCase()
 
 export const types = Object.freeze({
-  COMPONENT: Symbol.for('nano_spa.component'),
-  FRAGMENT : Symbol.for('nano_spa.fragment'),
-  HANDLER  : Symbol.for('nano_spa.handler'),
-  FUNCTION : Symbol.for('nano_spa.function')
+  COMPONENT: Symbol.for('abstract-ui.component'),
+  FRAGMENT : Symbol.for('abstract-ui.fragment'),
+  HANDLER  : Symbol.for('abstract-ui.handler'),
+  FUNCTION : Symbol.for('abstract-ui.function')
 })
 
 export const is_type = type => node => node.$type == types[type]
