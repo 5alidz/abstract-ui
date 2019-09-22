28797c22602e9d16dc5c8f9654be56ba9f6708d5
5alidz <kzack-dev@hotmail.com>
2019-09-22 14:03:31 +0300

    refactor plugins

--- src/validate/plugins.js
+++ src/validate/plugins.js
@@ -1,64 +1,79 @@
-const typeOf = object => Object.prototype.toString.call(object)
-  .replace(/[[\]]/g, '').split(' ')[1].toLowerCase()
+const get_type = type => Object.prototype.toString.call(type)
+const typeOf = type => get_type(type).replace(/[[\]]/g, '').split(' ')[1].toLowerCase()
+const get_name = prop => prop.split(':')[0]
+const msg_name = prop => prop.replace(':', ' ->')
 
 export default [
   (bus) => bus.register({
-    name: 'typeOf',
+    name: 'type_of: props',
     target: 'validation.props',
-    callback: (prop_name, validation, targets, plugin_name) => {
-      if(typeOf(targets['node.props'][prop_name]) !== 'undefined' && validation['typeOf']) {
-        if(validation['typeOf'] !== typeOf(targets['node.props'][prop_name])) {
+    callback: (prop_name, validation, targets, PLUGIN_NAME) => {
+      if(prop_name == '*') { return }
+      const target = targets['node.props'][prop_name]
+      const v_prop = validation[get_name(PLUGIN_NAME)]
+      if(typeOf(target) !== 'undefined' && v_prop) {
+        if(v_prop !== typeOf(target)) {
           bus.push('error', {
-            [`[ ${plugin_name} ] -> props <${prop_name}>`]: undefined,
-            '\texpected': validation['typeOf'],
-            '\trecived': typeOf(targets['node.props'][prop_name])
+            [`[ ${msg_name(PLUGIN_NAME)} -> "${prop_name}" ]`]: undefined,
+            '\texpected': v_prop,
+            '\trecived': typeOf(target)
           })
         }
       }
     }
   }),
   (bus) => bus.register({
-    name: 'required',
+    name: 'required: props',
     target: 'validation.props',
-    callback: (prop_name, validation, targets, plugin_name) => {
-      const is_required = Boolean(validation.required)
-      if(is_required && typeOf(targets['node.props'][prop_name]) == 'undefined') {
+    callback: (prop_name, validation, targets, PLUGIN_NAME) => {
+      const target = targets['node.props'][prop_name]
+      const v_prop = Boolean(validation[get_name(PLUGIN_NAME)])
+      if(v_prop && typeOf(target) == 'undefined') {
         bus.push('error', {
-          [`[ ${plugin_name} ] -> props <${prop_name}>`]: undefined,
+          [`[ ${msg_name(PLUGIN_NAME)} ->  "${prop_name}" ]`]: undefined,
           ...Object.entries(validation).reduce((accu, curr) => {
-            accu[`\t${curr[0]}`] = curr[1]
-            return accu
+            if(curr[0] == get_name(PLUGIN_NAME)) {
+              return accu
+            } else {
+              accu[`\t${curr[0]}`] = curr[1]
+              return accu
+            }
           }, {})
         })
       }
     }
   }),
   (bus) => bus.register({
-    name: 'children-typeOf',
+    name: 'type_of: children',
     target: 'validation.children',
-    callback: (key, value, targets, plugin_name) => {
+    callback: (key, value, targets, PLUGIN_NAME) => {
       const is_digit = /\d+/g.test(key)
+      const target = targets['node.children'][key]
+      const v_prop = value[get_name(PLUGIN_NAME)]
       if(is_digit) {
-        if(Boolean(value.typeOf) && value.typeOf !== typeOf(targets['node.children'][key])) {
+        if(Boolean(v_prop) && v_prop !== typeOf(target)) {
           bus.push('error', {
-            [`[ ${plugin_name} ] -> [${key}]`]: undefined,
-            '\texpected': value.typeOf,
-            '\trecived': typeOf(targets['node.children'][key])
+            [`[ ${msg_name(PLUGIN_NAME)} -> [${key}] ]`]: undefined,
+            '\texpected': v_prop,
+            '\trecived': typeOf(target)
           })
         }
       }
     }
   }),
   (bus) => bus.register({
-    name: 'wild-card-typeOf-prop',
+    name: 'type_of: wild-card-prop',
     target: 'node.props',
-    callback: (prop_name, prop_value, targets, plugin_name) => {
-      if(!targets[`validation.props`][prop_name] && targets['validation.props']['*']) {
-        if(targets['validation.props']['*'].typeOf !== typeOf(prop_value)) {
+    callback: (prop_name, prop_value, targets, PLUGIN_NAME) => {
+      const target = targets['validation.props'][prop_name]
+      const wild_target = targets['validation.props']['*']
+      const v_prop = prop_value
+      if(!target && wild_target) {
+        if(wild_target[get_name(PLUGIN_NAME)] !== typeOf(v_prop)) {
           bus.push('error', {
-            [`[ ${plugin_name} ] -> ${prop_name}`]: undefined,
-            [`value (${typeOf(prop_value)})`]: prop_value,
-            expected: targets['validation.props']['*'].typeOf
+            [`[ ${msg_name(PLUGIN_NAME)} -> "${prop_name}" ]`]: undefined,
+            [`\tvalue (${typeOf(prop_value)})`]: v_prop,
+            '\texpected': wild_target[get_name(PLUGIN_NAME)]
           })
         }
       }
