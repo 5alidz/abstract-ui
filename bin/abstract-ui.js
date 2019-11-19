e6a5855950ccbcb39be894803c8224077910d635
5alidz <kzack-dev@hotmail.com>
2019-11-19 06:50:58 +0300

    remove docs experimental

--- bin/abstract-ui.js
+++ bin/abstract-ui.js
@@ -1,40 +1,33 @@
 #!/usr/bin/env node
 
-const [,, command, ...args] = process.argv
+const [, , command, ...args] = process.argv;
 
-const START = require('../src/cli/start.js')
-const DOCS = require('../src/cli/docs.js')
-const BUILD = require('../src/cli/build.js')
+const START = require('../src/cli/start.js');
+const BUILD = require('../src/cli/build.js');
 
 const aliases = {
   '-p': 'port',
   '-d': 'docs',
   '-s': 'src'
-}
+};
 
 const args_obj = (() => {
-  const a_array = []
-  for(let i = 0; i < args.length; i += 2) {
-    a_array.push([args[i], args[i+1]])
+  const a_array = [];
+  for (let i = 0; i < args.length; i += 2) {
+    a_array.push([args[i], args[i + 1]]);
   }
   return a_array.reduce((acc, curr) => {
-    if(aliases[curr[0]]) {acc[aliases[curr[0]]] = curr[1]}
-    return acc
-  }, {})
-})()
+    if (aliases[curr[0]]) {
+      acc[aliases[curr[0]]] = curr[1];
+    }
+    return acc;
+  }, {});
+})();
 
-if(command == 'start') {
-  if(args[0] == 'docs') {
-    DOCS()
-    START({ src: 'docs' })
-  } else {
-    START(args_obj)
-  }
-} else if(command == 'build') {
-  BUILD(args_obj)
-} else if(command == 'docs') {
-  DOCS()
+if (command == 'start') {
+  START(args_obj);
+} else if (command == 'build') {
+  BUILD(args_obj);
 } else {
-  console.log(`command ${command} is not recognized by abstract-ui`)
+  console.log(`command ${command} is not recognized by abstract-ui`);
 }
-
